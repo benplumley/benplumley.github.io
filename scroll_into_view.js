541bbfda73eb29fe0eff6ef0beefbb203fa19828
Ben Plumley <benplumley@gmail.com>
2014-08-24 20:33:38 +0100

    Added click to scroll to index

--- scroll_into_view.js
+++ scroll_into_view.js
@@ -49,3 +49,28 @@ function showIt(eID) {
     }
     return false;
 }
+
+function showScreen(eID) {
+  var startY = currentYPosition();
+  var stopY = elmYPosition(eID) - 108;
+  var distance = stopY > startY ? stopY - startY : startY - stopY;
+  if (distance < 100) {
+      scrollTo(0, stopY); return;
+  }
+  var speed = Math.round(distance / 100);
+  if (speed >= 100) speed = 100;
+  var step = Math.round(distance / 25);
+  var leapY = stopY > startY ? startY + step : startY - step;
+  var timer = 0;
+  if (stopY > startY) {
+      for ( var i=startY; i<stopY; i+=step ) {
+          setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+          leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+      } return;
+  }
+  for ( var i=startY; i>stopY; i-=step ) {
+      setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+      leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+  }
+  return false;
+}
